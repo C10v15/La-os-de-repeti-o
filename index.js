3802585dcc834d43dc520a811c9cfde76eafddbd
Clovis Santiago <sant_sivolc@hotmail.com>
2022-09-29 15:10:59 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+alert("Olá! Para interagir com este conteúdo: 1º CLICK f12 no teclado 2º Vai abrir uma janela na lateral à direita no browser 3º No menu CLICK em CONSOLE 4º CLICK em ok")
+
+for(let i = 0; i < 11; i++){
+    console.log ('O número atual é', i)
+}
